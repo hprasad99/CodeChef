b2af9129a250157dc9e9043827cb66a5ebd104d8
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2020-10-12 12:43:59 +0530

    beginner

---
+++ FSQRT.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t ;
+    long n;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n;
+        cout << int(sqrt(n)) << "\n";
+    }
+    return 0;
+}
