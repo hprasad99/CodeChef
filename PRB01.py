2c12c337ffae7c2d73d2d7142586f91af46549e6
Himanshu Prasad <chatwith.himanshu27@gmail.com>
2020-10-27 02:51:48 +0530

    Primality test

--- PRB01.py
+++ PRB01.py
@@ -1,18 +1,19 @@
 # https://www.codechef.com/problems/PRB01
+import math
+
+
 def primal(n):
-    flag = 0
-    for i in range(4, n):
-        if(n % 2 == 0 or n % 3 == 0 or n % i == 0):
-            print("no")
-            flag = 1
-            break
-    if(flag == 0):
-        print("yes")
+    if(n < 2):
+        return "no"
+    for x in range(2, int(math.sqrt(n))+1):
+        if(n % x == 0):
+            return "no"
+    return "yes"
 
 
 if __name__ == "__main__":
     t = int(input())
     while(t):
         n = int(input())
-        primal(n)
+        print(primal(n))
         t -= 1
